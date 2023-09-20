2f5093ca551e8894f504584a4a653872008add74
Iuntevici Stanislav <eustace442@gmail.com>
2023-09-20 10:59:39 +0300

    CDK_CI_CD_Pipeline

---
+++ app-cdk/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+
+from app_cdk.app_cdk_stack import AppCdkStack
+from app_cdk.pipeline_cdk_stack import PipelineCdkStack
+from app_cdk.ecr_cdk_stack import EcrCdkStack
+
+
+app = cdk.App()
+
+ecr_stack = EcrCdkStack(app, 'ecr-stack')
+
+test_app_stack = AppCdkStack(
+    app,
+    'test-app-stack',
+    ecr_repository=ecr_stack.ecr_data
+)
+
+PipelineCdkStack(
+    app,
+    'pipeline-stack',
+    ecr_repository=ecr_stack.ecr_data,
+    test_app_fargate = test_app_stack.ecs_service_data
+)
+
+app.synth()
